57648d2eb58e7b99f03ded86860506f5ccd54051
Fabian Montoya <fabianmontoya2802@gmail.com>
2024-12-28 20:35:15 -0600

    feat: add Experience, Projects, and Studies components; update index layout and container styles

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 
 export default function Container({ children, ...props }: React.ComponentProps<'div'>) {
-  return <div {...props} className={cn(props.className, 'container mx-auto')}>{children}</div>
+  return <div {...props} className={cn(props.className, 'container mx-auto w-[320px] sm:w-[420px] lg:w-[860px]')}>{children}</div>
 }
 
