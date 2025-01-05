125c427236e026ffa9084951362990cdf508d406
Fabian Montoya <fabianmontoya2802@gmail.com>
2025-01-05 11:53:41 -0600

    feat: improve Container component layout for better responsiveness and adjust Projects grid spacing

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,18 @@
 import { cn } from "@/lib/utils";
 
-export default function Container({ children, ...props }: React.ComponentProps<'div'>) {
-  return <div {...props} className={cn(props.className, 'container mx-auto w-[320px] sm:w-[420px] lg:w-[860px]')}>{children}</div>
+export default function Container({
+  children,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      {...props}
+      className={cn(
+        props.className,
+        "container mx-auto w-full px-4 sm:w-[420px] lg:w-[860px]"
+      )}
+    >
+      {children}
+    </div>
+  );
 }
-
