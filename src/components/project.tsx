0b6e58a9b7f50d74636054b9b83c84811bb5dfdc
fabmnt <fabianmontoya2802@gmail.com>
2025-01-14 00:18:21 -0600

    feat: update text colors in Experience, Footer, Studies, and Project components for improved readability in dark mode

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -49,7 +49,7 @@ export function Project({ project }: { project: Project }) {
             </div>
           </header>
           <div>
-            <p className="text-sm font-thin text-muted-foreground dark:text-slate-300">
+            <p className="text-sm font-thin text-slate-700 dark:text-slate-300">
               {project.description}
             </p>
             <div className="flex flex-wrap gap-2 mt-2">
